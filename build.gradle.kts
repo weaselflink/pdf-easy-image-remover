2c7a25b6e207c50d057f3613fce89f4c4e525756
weaselflink <stefan@stefanbissell.de>
2022-07-26 12:22:10 +0200

    keep dependencies up to date

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.itextpdf:itext7-core:7.2.2")
+    implementation("com.itextpdf:itext7-core:7.2.3")
     implementation("org.slf4j:slf4j-nop:1.7.36")
     implementation("com.github.jai-imageio:jai-imageio-core:1.4.0")
     implementation("com.github.jai-imageio:jai-imageio-jpeg2000:1.4.0")
